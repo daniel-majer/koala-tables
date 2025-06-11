7f7b6333a9a0ab02c4c4b5bb34d2a0afedd735b5
daniel-majer <dano.majer@protonmail.com>
2025-06-11 21:42:01 +0200

    edit recursive function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,22 +1,30 @@
 export const deleteRecursive = (item, rowRef) => {
-  let node = item
-
-  if (node.data === rowRef) {
+  if (item.data === rowRef) {
     return null
   }
 
-  if (node.children) {
-    for (const key in node.children) {
-      if (node.children[key]?.records) {
-        node.children[key].records = node.children[key].records
-          .map(childNode => deleteRecursive(childNode, rowRef))
+  const newItem = { ...item }
+
+  if (newItem.children) {
+    const newChildren = {}
+
+    for (const key in newItem.children) {
+      if (newItem.children[key]?.records) {
+        const updatedRecords = newItem.children[key].records
+          .map(child => deleteRecursive(child, rowRef))
           .filter(Boolean)
-        if (!node.children[key].records.length) {
-          node.children = {}
+
+        if (updatedRecords.length) {
+          newChildren[key] = {
+            ...newItem.children[key],
+            records: updatedRecords,
+          }
         }
       }
     }
+
+    newItem.children = Object.keys(newChildren).length > 0 ? newChildren : {}
   }
 
-  return node
+  return newItem
 }
